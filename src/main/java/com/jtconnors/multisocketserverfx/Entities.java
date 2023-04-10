968fc31031e00e6a46c9b171b0694ff10dc89020
JP <jaymilparikh@gmail.com>
2023-04-09 21:08:48 -0500

    April 9 9:08pm

---
+++ src/main/java/com/jtconnors/multisocketserverfx/Entities.java
@@ -0,0 +1,7 @@
+package com.jtconnors.multisocketserverfx;
+
+public class Entities {
+
+
+
+}
--- src/main/java/com/jtconnors/multisocketserverfx/FXMLDocumentController.java
+++ src/main/java/com/jtconnors/multisocketserverfx/FXMLDocumentController.java
@@ -17,6 +17,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 
 import com.jtconnors.socketfx.FxMultipleSocketServer;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 /**
  *
@@ -36,6 +39,8 @@ public class FXMLDocumentController implements Initializable {
     private Button connectButton;
     @FXML
     private Button disconnectButton;
+    @FXML
+    private Label lblPort;
     @FXML
     private TextField portTextField;
     @FXML
@@ -59,22 +64,22 @@ public class FXMLDocumentController implements Initializable {
             case DISCONNECTED:
                 connectButton.setDisable(false);
                 disconnectButton.setDisable(true);
-                sendButton.setDisable(true);
-                sendTextField.setDisable(true);
+//                sendButton.setDisable(true);
+//                sendTextField.setDisable(true);
                 connectionsLabel.setText("Not connected");
                 break;
             case WAITING:
                 connectButton.setDisable(true);
                 disconnectButton.setDisable(false);
-                sendButton.setDisable(false);
-                sendTextField.setDisable(false);
+//                sendButton.setDisable(false);
+//                sendTextField.setDisable(false);
                 connectionsLabel.setText("Waiting for connections");
                 break;
             case CONNECTED:
                 connectButton.setDisable(true);
                 disconnectButton.setDisable(false);
-                sendButton.setDisable(false);
-                sendTextField.setDisable(false);
+//                sendButton.setDisable(false);
+//                sendTextField.setDisable(false);
                 int numConnections = socketServer.getListenerCount();
                 StringBuilder connectionsSB
                         = new StringBuilder(numConnections + " connection");
@@ -82,6 +87,24 @@ public class FXMLDocumentController implements Initializable {
                     connectionsSB.append("s");
                 }
                 connectionsLabel.setText(new String(connectionsSB));
+
+                for (int i = 0; i < map.length; i++) {
+                    for (int j = 0; j < map[0].length; j++) {
+                        map[i][j] = new Button();
+                        map[i][j].setPrefSize(1, 1);
+                        map[i][j].setPrefHeight(1);
+                        map[i][j].setPrefWidth(1);
+//                map[i][j] = new ImageView();
+//                map[i][j].setEffect(c);
+//                map[i][j].setFitHeight(70);
+//                map[i][j].setFitWidth(70);
+                        if (i == 0 || j == 0 || i == 49 || j == 29){
+                            map[i][j].setStyle("-fx-background-color: black");
+                        }
+                        MAP.add(map[i][j], j, i);
+                    }
+                }
+
                 break;
         }
     }
@@ -108,29 +131,29 @@ public class FXMLDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         displayState(ConnectionDisplayState.DISCONNECTED);
-        sentMsgsData = FXCollections.observableArrayList();
-        sentMsgsListView.setItems(sentMsgsData);
-        sentMsgsListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        sentMsgsListView.setOnMouseClicked((Event event) -> {
-            String selectedItem
-                    = sentMsgsListView.getSelectionModel().getSelectedItem();
-            if (selectedItem != null && !selectedItem.equals("null")) {
-                selectedTextField.setText("Sent: " + selectedItem);
-                lastSelectedListView = sentMsgsListView;
-            }
-        });
-
-        rcvdMsgsData = FXCollections.observableArrayList();
-        rcvdMsgsListView.setItems(rcvdMsgsData);
-        rcvdMsgsListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        rcvdMsgsListView.setOnMouseClicked((Event event) -> {
-            String selectedItem
-                    = rcvdMsgsListView.getSelectionModel().getSelectedItem();
-            if (selectedItem != null && !selectedItem.equals("null")) {
-                selectedTextField.setText("Received: " + selectedItem);
-                lastSelectedListView = rcvdMsgsListView;
-            }
-        });
+//        sentMsgsData = FXCollections.observableArrayList();
+//        sentMsgsListView.setItems(sentMsgsData);
+//        sentMsgsListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+//        sentMsgsListView.setOnMouseClicked((Event event) -> {
+//            String selectedItem
+//                    = sentMsgsListView.getSelectionModel().getSelectedItem();
+//            if (selectedItem != null && !selectedItem.equals("null")) {
+//                selectedTextField.setText("Sent: " + selectedItem);
+//                lastSelectedListView = sentMsgsListView;
+//            }
+//        });
+//
+//        rcvdMsgsData = FXCollections.observableArrayList();
+//        rcvdMsgsListView.setItems(rcvdMsgsData);
+//        rcvdMsgsListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+//        rcvdMsgsListView.setOnMouseClicked((Event event) -> {
+//            String selectedItem
+//                    = rcvdMsgsListView.getSelectionModel().getSelectedItem();
+//            if (selectedItem != null && !selectedItem.equals("null")) {
+//                selectedTextField.setText("Received: " + selectedItem);
+//                lastSelectedListView = rcvdMsgsListView;
+//            }
+//        });
 
         portTooltip = new Tooltip("Port number cannot be modified once\n" +
         "the first connection attempt is initiated.\n" +
@@ -185,4 +208,14 @@ public class FXMLDocumentController implements Initializable {
         socketServer.shutdown();
     }
 
+//    ImageView[][] map = new ImageView[200][100];
+
+    Button[][] map = new Button[50][30];
+
+    @FXML
+    private GridPane MAP;
+
+    @FXML
+    private void initialize(){
+    }
 }

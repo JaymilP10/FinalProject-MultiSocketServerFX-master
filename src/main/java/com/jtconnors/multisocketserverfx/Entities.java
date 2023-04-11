cb63e12b4072651117b45db502a6c6179ddb22e3
JP <jaymilparikh@gmail.com>
2023-04-10 21:48:04 -0500

    April 10 9:47pm

--- src/main/java/com/jtconnors/multisocketserverfx/Entities.java
+++ src/main/java/com/jtconnors/multisocketserverfx/Entities.java
@@ -1,5 +1,6 @@
 package com.jtconnors.multisocketserverfx;
 
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 
 import java.lang.reflect.Array;
@@ -12,13 +13,24 @@ public class Entities {
     int level;
     int range;
     int health;
+    int maxHealth;
     int healthIncrease;
     int speed;
+    int xLoc;
+    int yLoc;
+
+    ProgressBar healthBar = new ProgressBar(1);
 
     Weapon primary;
     Weapon secondary;
 
     public Entities(int level, int range, int health, int healthIncrease, int speed, Weapon primary, Weapon secondary){
 
+    }
+
+    public void changeHealth(int amount){
+        health += amount;
+        double progress = health/maxHealth;
+        healthBar.setProgress(progress);
     }
 }
